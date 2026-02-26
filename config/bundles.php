5840cf32683393e689825e664905b2beea97c6fb
ilef2003 <ilefyousfi7@gmail.com>
2026-02-26 04:46:29 +0100

    feat: Add StofDoctrineExtensionsBundle and NelmioApiDocBundle for better grade

--- config/bundles.php
+++ config/bundles.php
@@ -19,6 +19,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    CalendarBundle\CalendarBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
